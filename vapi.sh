ed8b5d778b77c5b1101a72427e67ceeb95313eb6
vaderus <92164449+vaderus@users.noreply.github.com>
2021-12-15 22:05:34 +0000

    Add files via upload

--- vapi.sh
+++ vapi.sh
@@ -1,17 +1,36 @@
 #!/bin/sh
+
+#usar doas ou sudo
 echo "Do you want to use (doas) or (sudo)?"
 read doas
+echo "usando ${doas} ..."
 
-mkdir /home/vader/media/img/wall/
+#habilitar repos arch
+echo "Do you want the script to enable Arch linux repos in pacman.conf?(yes/no)"
+read repo
+case $repo in
+	[yes]* ) $doas pacman -S artix-archlinux-support && $doas pacman-key --populate archlinux &&
+$doas echo "
+[lib32]
+Include = /etc/pacman.d/mirrorlist
+[community]
+Include = /etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf;;
+	[no]* ) echo "ok, im not doing that" && sleep 2s;
+esac
+
+#criar pasta de wallpapers
+mkdir ~/media/img/wall/
+
 #install some packages
 ${doas} pacman -Syu --needed git xorg-server xorg-xrandr xorg-xinit libx11 libxinerama ttf-hack bspwm polybar doas ranger sxhkd vim zsh xwallpaper pipewire pipewire-pulse pipewire-media-session xclip scrot
+
 #git clone other packages
 git clone https://aur.archlinux.org/picom-ibhagwan-git.git /tmp/picom.git &
 git clone https://aur.archlinux.org/libxft-bgra.git /tmp/libxf-bgra.git
 cd /tmp/picom.git
-makepkg -si
-cd /tmp/libxft-bgra.git
-makepkg -si
+makepkg -si --needed
+cd /tmp/libxft-bgra.git 
+makepkg -si --needed
 #git clone the dot files
 git clone https://github.com/vaderus/artix-configs ~/artix-configs
 cp -r ~/artix-configs/.sources ~/.sources
@@ -23,11 +42,11 @@ cp -r ~/artix-configs/desktop/.xinitrc ~/.xinitrc
 mv ~/.config/.vimrc ~/.vimrc
 mv ~/.config/.zshrc ~/.zshrc
 
-${doas} make clean install -C ~/.souces/st
-${doas} make clean install -C ~/.souces/dmenu
+${doas} make clean install -C ~/.sources/st
+${doas} make clean install -C ~/.sources/dmenu
 
 clear
-echo"All done! do you want to start xorg now?(yes/no)"
+echo "All done! do you want to start xorg now?'(yes/no)'"
 read yn
 case $yn in
 	[yes]* ) startx;;
