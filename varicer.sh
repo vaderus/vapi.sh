81790949feae8bef0d2bc2d7d6dac5913cb19b0e
vaderus <92164449+vaderus@users.noreply.github.com>
2021-12-21 00:52:37 +0000

    Add files via upload

--- varicer.sh
+++ varicer.sh
@@ -1,21 +1,27 @@
 #!/bin/sh
-read -p "How do you want to provide root acess? (sudo/doas): " doas
 
+# pergunta se quer user doas ou sudo
+read -p "How do you want to provide root acess? (sudo/doas): " doas
+clear
+
+#pergunta se quer clonar os dot files
 read -p "Do you want me to git clone the dot files? (Y/n): " clone
 case $clone in
-	y)
-		git clone https://github.com/vaderus/void-configs ~/void-configs;
-		cp -r ~/void-configs/desktop/.config ~/.config;
-		cp -r ~/void-configs/desktop/.scripts ~/.scripts;
-		$doas cp -r ~/void-configs/desktop/.themes /usr/share/.themes;
-		mkdir -p ~/media/img/
-		cp -r ~/void-configs/desktop/wall ~/media/img/wall/;
-		cp -r ~/void-configs/desktop/.xinitrc ~/.xinitrc;
-		cp ~/.config/.vimrc ~/.vimrc;
-		cp ~/.config/.zshrc ~/.zshrc;;
-	n)
-		echo "ok, im not doing that...";;
-	*)
+	y) #caso a resposta for sim faça isso:
+		git clone https://github.com/vaderus/void-configs ~/void-configs; #clona void-configs
+		cp -r ~/void-configs/desktop/.config ~/.config; #move o .config
+		cp -r ~/void-configs/desktop/.scripts ~/.scripts; #move o .scripts
+		$doas cp -r ~/void-configs/desktop/.themes /usr/share/.themes; #move o .themes
+		mkdir -p ~/media/img/ #cria a pasta de wallpaper
+		cp -r ~/void-configs/desktop/wall ~/media/img/wall/; #move a pasta de wallpaper
+		cp -r ~/void-configs/desktop/.xinitrc ~/.xinitrc; #move o .xinitrc
+		cp ~/.config/.vimrc ~/.vimrc; #move o vimrc
+		cp ~/.config/.zshrc ~/.zshrc; #move o zshrc
+		clear;;
+	n) #se a resposta for não echo isso:
+		echo "ok, im not doing that...";
+		clear;;
+	*) #se a resposta for outra coisa faça o mesmo que sim
 		git clone https://github.com/vaderus/void-configs ~/void-configs;
 		cp -r ~/void-configs/.sources ~/.sources;
 		cp -r ~/void-configs/desktop/.config ~/.config;
@@ -25,68 +31,96 @@ case $clone in
 		cp -r ~/void-configs/desktop/wall ~/media/img/wall/;
 		cp -r ~/void-configs/desktop/.xinitrc ~/.xinitrc;
 		cp ~/.config/.vimrc ~/.vimrc;
-		cp ~/.config/.zshrc ~/.zshrc;;
+		cp ~/.config/.zshrc ~/.zshrc;
+		clear;;
 esac	
 
-read -p "Do you want me to install the desktop packages now? [xorg,bspwm,etc] (Y/n): " packages
+read -p "Do you want me to install the desktop packages now? [xorg,bspwm,etc] (Y/n): " packages #pergunta se quer instalar o desktop
 case $packages in
-	y)
-		clear;
+	y) #se sim instale isso:
 		echo "installing packages ...";
 		sleep 3;
-		$doas xbps-install -Syu git xorg-minimal xinit font-hack-ttf libXinerama-devel libX11-devel libXft-devel harfbuzz-devel bspwm sxhkd polybar vim base-devel xrandr setxkbmap xwallpaper xset zsh dbus elogind pipewire;;
-	n)
-		clear;
-		echo "ok, im not going to install any packages";;
-	*)
-		clear;
+		$doas xbps-install -Syu git xorg-minimal xinit font-hack-ttf libXinerama-devel libX11-devel libXft-devel harfbuzz-devel bspwm sxhkd polybar vim base-devel xrandr setxkbmap xwallpaper xset zsh dbus elogind pipewire;
+		clear
+	n) #se não echo isso:
+		echo "ok, im not going to install any packages";
+		sleep 3;
+		clear;;
+	*) #se responder outra coisa faça o mesmo que sim
 		echo "installing packages ...";
 		sleep 3;
-		$doas xbps-install -Syu git xorg-minimal xinit font-hack-ttf libXinerama-devel libX11-devel libXft-devel harfbuzz-devel bspwm sxhkd polybar vim base-devel xrandr setxkbmap xwallpaper xset zsh dbus elogind pipewire;;
+		$doas xbps-install -Syu git xorg-minimal xinit font-hack-ttf libXinerama-devel libX11-devel libXft-devel harfbuzz-devel bspwm sxhkd polybar vim base-devel xrandr setxkbmap xwallpaper xset zsh dbus elogind pipewire;
+		clear;;
 esac
 
-read -p "do you want me to compile dmenu and st? (Y/n): " compile
+read -p "do you want me to compile dmenu and st? (Y/n): " compile #pergunta se quer instalar dmenu e st
 case $compile in
-	y)
-		read -p "Where do you want me to clone it? (e.g /home/user/.sources): " sourcesdir;
-		mkdir -p $sourcesdir
-		git clone https://github.com/lukesmithxyz/st $sourcesdir/st;
-		git clone https://git.suckless.org/dmenu $sourcesdir/dmenu;
-		cp ~/void-configs/desktop/st-config.h $sourcesdir/st/config.h;
-		cp ~/void-configs/desktop/dmenu-config.h $sourcesdir/dmenu/config.h;
-		$doas make clean install -C $sourcesdir/st;
-		$doas make clean install -C $sourcesdir/dmenu;;
-	n)
+	y) #se sim faz iso:
+		read -p "Where do you want me to clone it? (e.g /home/user/.sources): " sourcesdir; #pergunta onde clonar as sources
+		mkdir -p $sourcesdir #cria a pasta de sources
+		git clone https://github.com/lukesmithxyz/st $sourcesdir/st; #clone o st do luke
+		git clone https://git.suckless.org/dmenu $sourcesdir/dmenu; #clona o dmenu do sucklesss
+		cp ~/void-configs/desktop/st-config.h $sourcesdir/st/config.h; #move o config.h para st
+		cp ~/void-configs/desktop/dmenu-config.h $sourcesdir/dmenu/config.h; #move o config.h para dmenu
+		$doas make clean install -C $sourcesdir/st; #instala sst
+		$doas make clean install -C $sourcesdir/dmenu; #instala dmenu
+		clear;;
+	n) #se não echo isso:
 		echo "ok, im not doing it";
-		sleep 3;;
-	*)	
-		read -p "Where do you want me to clone it? (e.g ~/.sources) :" sourcesdir;
-		mkdir -p $sourcesdir
-		git clone https://github.com/lukesmithxyz/st $sourcesdir/st;
-		git clone https://git.suckless.org/dmenu $sourcesdir/dmenu;
-		cp ~/void-configs/desktop/st-config.h $sourcesdir/st/config.h;
-		cp ~/void-configs/desktop/dmenu-config.h $sourcesdir/dmenu/config.h;
-		$doas make clean install -C $sourcesdir/st;
-		$doas make clean install -C $sourcesdir/dmenu;;
+		sleep 3;
+		clear;;
+	*) #se responder outra coisa faça o mesmo que sim	
+		read -p "Where do you want me to clone it? (e.g /home/user/.sources): " sourcesdir; #pergunta onde clonar as sources
+		mkdir -p $sourcesdir #cria a pasta de sources
+		git clone https://github.com/lukesmithxyz/st $sourcesdir/st; #clone o st do luke
+		git clone https://git.suckless.org/dmenu $sourcesdir/dmenu; #clona o dmenu do sucklesss
+		cp ~/void-configs/desktop/st-config.h $sourcesdir/st/config.h; #move o config.h para st
+		cp ~/void-configs/desktop/dmenu-config.h $sourcesdir/dmenu/config.h; #move o config.h para dmenu
+		$doas make clean install -C $sourcesdir/st; #instala sst
+		$doas make clean install -C $sourcesdir/dmenu; #instala dmenu
+		clear;;
 esac
 
-read -p "do you want me to start elogind and dbus? [runit] (Y/n)" services
+read -p "do you want me to start elogind and dbus? [runit] (Y/n)" services #pergunta se que iniciar o dbus e elogind (para pipewire)
 case $services in
-	y)
-		$doas ln -s /etc/sv/elogind /var/service/;
-		$doas sv up elogind;
-		$doas ln -s /etc/sv/dbus /var/service/;
-		$doas sv up dbus;;
-	n)
+	y) #se sim faça:
+		if [ -e "/var/service/elogind" ] #se o link já existe apenas inicie o service
+		then
+			doas sv up elogind;
+		else #se não existe, cria o link e inicia o service
+			doas ln -s /etc/sv/elogind /var/service/;
+			doas sv up elogind;
+		fi
+
+		if [ -e "/var/service/dbus" ] #se o link já existe apenas inicie o service
+		then
+			doas sv up dbus;
+		else #se não existe, cria o link e inicia o service
+			doas ln -s /etc/sv/dbus /var/service/;
+			doas sv up dbus;
+		fi;;
+	n) #se não echo isso:
 		echo "ok im not doing it";
 		sleep 3;;
-	*)
-		$doas ln -s /etc/sv/elogind /var/service/;
-		$doas sv up elogind;
-		$doas ln -s /etc/sv/dbus /var/service/;
-		$doas sv up dbus;;
+	*) #se responder outra coisa faça o mesmo que sim
+		if [ -e "/var/service/elogind" ]
+		then
+			doas sv up elogind;
+		else
+			doas ln -s /etc/sv/elogind /var/service/;
+			doas sv up elogind;
+		fi
+		if [ -e "/var/service/dbus" ]
+		then
+			doas sv up dbus;
+		else
+			doas ln -s /etc/sv/dbus /var/service/;
+			doas sv up dbus;
+		fi;;
 esac
 
+#muda o shell
+clear
 read -p "do you want to change your shell to zsh? (Y/n): " zsh
 case $zsh in
 	y)
@@ -95,6 +129,8 @@ case $zsh in
 		echo "ok im not chaging the shell";;
 esac
 
+#pergunta se quer iniciar o xorg
+clear
 read -p "everythig is done, do you want to start xorg now? (Y/n): " xorg
 case $xorg in
 	y)
